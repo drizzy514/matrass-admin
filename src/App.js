eb4d0a192c274ca2d73ce3fe52deca0a01dc07a1
abduhakim <muhtorovabduhakim59@gmail.com>
2022-05-06 00:02:13 +0500

    admin+

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import {Route, Switch}  from "react-router-dom";
 
 
 // pages
-import Home from "./pages/Home/Home"
 import Admin from './pages/Admin/Admin'
 import Login from './pages/Login/Login'
 // routes
 import Private from './Routes/Private';
-import Public from './Routes/Public'
 
 function App() {
 return <>
